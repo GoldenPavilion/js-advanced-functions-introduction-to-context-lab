cad94d7aac370a8ef25844f4600eb6eb4c52ff44
GoldenPavilion <rcorystine@gmail.com>
2021-08-24 18:53:00 -0400

    updated functions with arguments and descriptive comments as a guide

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-function createEmployeeRecord(){
-
+function createEmployeeRecord(firstName, familyName, title, payRatePerHour){
+    // return Object "employee" {firstName, familyName, title, payRatePerHour, timeInEvents, timeOutEvents}
+    // Load Array elements into corresponding Object properties.
+    // Initialize empty Arrays on the properties timeInEvenst and timeOutEvents
+    // .map?
 }
 
-function createEmployeeRecords(){
-
+function createEmployeeRecords(employees){
+    // return Array of Objects
+    // Convert each nested Array into an employee record using createEmployeeRecord
+    // Accumulate it to a new Array
+    // .reduce?
 }
 
-function createTimeInEvent(){
-
+function createTimeInEvent(employee, dateStamp){
+    // returns the employee record
+    // Add an Object with keys to the timeInEvents Arrays on the record Object
+    // type - set to "TimeIn"
+    // hour - derived from the arg
+    // date - derived from the arg
 }
 
-function createTimeOutEvent(){
-
+function createTimeOutEvent(employee, dateStamp){
+    // returns the employee record
+    // Add an Object with keys to the timeOutEvents Arrays on the record Object
+    // type - set to "TimeOut"
+    // hour - derived from the arg
+    // date - derived from the arg
 }
 
-function findEmployeeByFirstName(){
-
+function hoursWorkedOnDate(employee, dateStamp){
+    // return hours worked, as an integer
+    // given a date, find the number of hours elapsed between the date's timeInEvents and timeOutEvent
 }
 
-function calculatePayroll(){
-    
+function wagesEarnedOnDate(employee, dateStamp){
+    // returns pay owed
+    // Using hoursWorkedOnDate, multiply the hours by the record's payRate to determine the amount owed.
+    // Amount should be returned as a number
+}
+
+function allWagesFor(employee){
+    // returns pay owed for all dates
+    // using wagesEarnedOnDate, accumulate the value of all dates worked by the employee in the record used as context
+    // Aamount should be returned as a number
+    // You will need to find the available dates somehow...
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    // returns matching record or undefined
+    // test the firstName field for a match with the firstName argument
+}
+
+function calculatePayroll(employee){
+    // returns sum of pay owed to all employees for all dates, as a number
+    // using wagedEarnedOnDate, accumulate the value of all dates worked by the employee in the record used as context
+    // Anount should be returned as a number
 }
